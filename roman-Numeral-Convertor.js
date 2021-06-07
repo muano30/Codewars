dcb4269fdb7537e45242ff00f18d669cc5f8971a
muano30 <muanonevhufumba@gmail.com>
2021-06-07 12:46:01 +0200

    roman-Numeral-Convetor

---
+++ roman-Numeral-Convertor.js
@@ -0,0 +1,37 @@
+// Roman Numeral Converter
+
+// Convert the given number into a roman numeral.
+
+// All roman numerals answers should be provided in upper-case.
+
+function convertToRoman(num) {
+    let decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romanNumeral = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I"
+    ];
+  
+    let romanized = "";
+  
+    for (let index = 0; index < decimalValue.length; index++) {
+      while (decimalValue[index] <= num) {
+        romanized += romanNumeral[index];
+        num -= decimalValue[index];
+      }
+    }
+  
+    return romanized;
+  };
+  
+  console.log(convertToRoman(1023));
